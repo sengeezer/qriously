b3eb5fbecdbd62020742dff61975bbab1289e970
Fabio Albertin <albertin@gmail.com>
2018-07-23 23:18:16 +0100

    Laid groundwork for Express API

--- src/client/index.js
+++ src/client/index.js
@@ -3,3 +3,32 @@ import 'babel-polyfill';
 import { APP_CONTAINER_SELECTOR } from '../shared/config';
 
 document.querySelector(APP_CONTAINER_SELECTOR).innerHTML = '<h1>Hello World!</h1>';
+
+/*
+API call example:
+
+import 'isomorphic-fetch';
+
+import { helloEndpointRoute } from '../../shared/routes';
+
+const callAPI = () => {
+  return fetch(helloEndpointRoute(num), { method: 'GET' })
+    .then((res) => {
+      if (!res.ok) {
+        throw Error(res.statusText);
+      }
+
+      return res.json()
+    })
+    .then((data) => {
+      if (!data.serverMessage) {
+        throw Error('No message received');
+      }
+      // do something with the data here
+    })
+    .catch(() => {
+      // catch the error
+    });
+};
+
+*/
